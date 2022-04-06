85cfea5f798aafb5592d2a1714b62b80516c5898
reeidi <reeidi@abv.bg>
2022-04-06 14:24:38 +0300

    Add components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { SliderHeaderComponent } from './components/slider-header/slider-header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    SliderHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
